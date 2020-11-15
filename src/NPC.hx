aa7574e54079175909475925eac14b09f5cf95de
Tyler McCarthy <tweedlitweedli@gmail.com>
2020-11-15 16:12:49 -0600

    walk anim

--- src/NPC.hx
+++ src/NPC.hx
@@ -74,6 +74,7 @@ class NPC extends FlxSprite {
         }
 
         animation.add('stand', [0]);
+        animation.add('walk', [4, 4, 0, 5, 5, 0], 6);
         animation.add('breathe', [0, 0, 0, 1, 2, 2, 1], 8);
 
         animation.play('breathe');
